b9b8fa74bcb1bb169da1ab0ff8190ce6a265be51
Paulius Aleksiunas <paulius.aleksiunas@gmail.com>
2015-11-06 17:02:10 +0200

    add has code action

--- src/DTR/DTRBundle/Controller/DefaultController.php
+++ src/DTR/DTRBundle/Controller/DefaultController.php
@@ -3,8 +3,10 @@
 namespace DTR\DTRBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Validator\Tests\Fixtures\Entity;
 
 class DefaultController extends Controller
 {
@@ -18,5 +20,25 @@ class DefaultController extends Controller
             'views/default/name.html.twig',
             array('name' => $name)
         );
+    }
+
+    public function hashAction($event)
+    {
+        // Generating unique hash code
+        $uniqueHash = uniqid();
+
+        // Object responsible for handling the process of persisting objects to the database
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // "Managing" the $event object, this doesn't cause a query to the database
+        $entityManager->persist($event);
+
+        // $event objest is persisted to the database
+        $entityManager->flush();
+
+        return $this->render(
+            'views/default/id.html.twig',
+            array('uniqueHash' => $uniqueHash
+        ));
     }
 }
