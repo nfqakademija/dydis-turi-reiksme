3dd902eeee6d9d115fee509f42c4b27d88432e11
Pavel Šliachtovič <p.sliacht@gmail.com>
2015-11-14 15:03:40 +0200

    Initial

---
+++ src/DTR/DTRBundle/Form/ShopType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DTR\DTRBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ShopType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('shoptype')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'DTR\DTRBundle\Entity\Shop'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'dtr_dtrbundle_shop';
+    }
+}
