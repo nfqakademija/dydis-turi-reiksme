7d8839f254869f8a48b93f8a44e278509bf27501
Paulius Aleksiunas <paulius.aleksiunas@gmail.com>
2015-11-19 18:17:04 +0200

    add DoctrineFixture for Shop entity

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
@@ -37,6 +38,6 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return '/tmp/cache';
+        return '/tmp/vilnius4';
     }
 }
