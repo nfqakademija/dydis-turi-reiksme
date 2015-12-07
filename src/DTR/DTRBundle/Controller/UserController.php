69d41e7ee51d72e1c3d7a78f948ab87ae8167a8c
Justas Bieliauskas <justyvas0@gmail.com>
2015-12-07 18:44:41 +0200

    Add new fields to /info tables.

--- src/DTR/DTRBundle/Controller/UserController.php
+++ src/DTR/DTRBundle/Controller/UserController.php
@@ -24,7 +24,6 @@ class UserController extends Controller
         $doctrine = $this->getDoctrine();
 
         $hosted_events = $doctrine->getRepository('DTRBundle:Event')->findHostedEvents($user);
-        //$participating_events = $doctrine->getRepository('DTRBundle:Event')->findParticipatingEvents($user);
         $participations = $doctrine->getRepository('DTRBundle:Member')->findParticipations($user);
 
         return $this->render('user/event_info.html.twig', compact('hosted_events', 'participations'));
--- src/DTR/DTRBundle/Repository/EventRepository.php
+++ src/DTR/DTRBundle/Repository/EventRepository.php
@@ -32,21 +32,9 @@ class EventRepository extends EntityRepository
     }
 
     /**
-     * @param User $user
+     * @param $hash
      * @return array
      */
-    public function findParticipatingEvents(User $user)
-    {
-        return $this
-            ->getEntityManager()
-            ->createQuery('SELECT e.hash, e.name, m.debt
-                           FROM DTRBundle:Event e
-                           JOIN e.members m
-                           WHERE m.user = :user
-                           AND m.is_host = false')
-            ->setParameter('user', $user)
-            ->getResult();
-    }
     public function findByHash($hash)
     {
         return $this->getEntityManager()
